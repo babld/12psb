5708f6ed1641aab8fc6b681e9857d0bb4c9a6f02
pash <pashk0s@yandex.ru>
2017-03-29 15:42:05 +0700

    add footer-form block

---
+++ frontend/views/blocks/footer-form.php
@@ -0,0 +1,30 @@
+<div class="footer-form" id="4">
+    <div class="layout-width">
+        <div class="footer-form-block">
+            <div class="footer-manager-wrap">
+                <img src="../i/manager.png"/>
+                <div class="footer-managet-notice">Дмитрий<br/>Менеджер по продаже стендов</div>
+            </div>
+            <div class="footer-form-wrap">
+                <div class="footer-form-title">
+                    <span>Нужна консультация</span> по выбору оборудования для <br/>тестирования ТНВД и Common Rail?
+                </div>
+                <div class="form-wrapper">
+                    <form class="feedback-form" onsubmit="ga('send', 'event', 'CALLBACK', '2'); yaCounter24717443.reachGoal('CALLBACK_BOT'); return true;">
+                        <div class="feedback-input-wrapper">
+                            <div class="feedback-input-text">Ваше сообщение:</div>
+                            <textarea class="feedback-input feedback-textarea" placeholder="Не обязательно" name="mess"></textarea>
+                        </div>
+                        <div class="feedback-phone-wrapper">
+                            <div class="feedback-input-text">Ваш телефон:</div>
+                            <i>+7</i>
+                            <input type="text" name="phone" class="feedback-input feedback-phone" placeholder="(___) ___ __ __">
+                        </div>
+                        <input type="submit" name="submit" class="feedback-submit" value="Получить консультацию"/>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
