955a0972e03de2d3a1effa94d6c9f4fccace7a3b
pash <pashk0s@yandex.ru>
2017-03-29 15:38:47 +0700

    add blocks

---
+++ frontend/views/blocks/certs.php
@@ -0,0 +1,14 @@
+<div class="certs">
+    <div class="layout-width">
+        <a href="i/cert1-big.jpg" rel="fancybox-button" class="lbox" alt="Сертификат" title="Сертификат">
+            <img class="" alt="Сертификат" title="Сертификат" src="">
+        </a>
+        <a href="i/cert2-big.jpg" rel="fancybox-button" class="lbox" alt="Сетрификат" title="Сетрификат">
+            <img class="" alt="Сетрификат" title="Сетрификат" src="">
+        </a>
+        <a href="i/cert3-big.jpg" rel="fancybox-button" class="lbox" alt="Сертификат на китайском языке" title="Сертификат на китайском языке">
+            <img class="" alt="Сертификат на китайском языке" title="Сертификат на китайском языке" src="">
+        </a>
+    </div>
+    <div class="certs-bottom-line"></div>
+</div>
---
+++ frontend/views/blocks/footer.php
@@ -0,0 +1,27 @@
+<div class="footer">
+    <div class="layout-width">
+        <div class="clear"></div>
+        <div class="footer-info">
+            <div class="footer-company-info">
+                <div class="footer-company-name">ГК &laquo;СибирьТрансАзия&raquo;</div>
+                <div class="footer-logo"></div>
+            </div>
+            <div class="footer-company-cities">
+                <ul class="footer-fillials">
+                    <li data-city="nsk" class='active'>Новосибирск</li>
+                    <li data-city="omsk" >Омск</li>
+                    <li data-city="ekb" >Екатеринбург</li>
+                    <li data-city="msk" >Москва</li>
+                    <li data-city="spb" >Санкт-Петербург</li>
+                </ul>
+            </div>
+            <div class="footer-right">
+                <div class="footer-contacts">
+                    <div class="footer-contacts-phone">8-383-207-8860</div>
+                    <div class="footer-contacts-address">пр. Дзержинского, 1/1, оф. 71</div>
+                </div>
+                <div class="footer-slogan">Продажа стендов ТНВД и Common <br/>Rail в наличии и под заказ</div>
+            </div>
+        </div>
+    </div>
+</div>
