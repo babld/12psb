0c4decb339b9ef0f1901e5a453f473849dd81352
pash <pashk0s@yandex.ru>
2017-04-24 11:55:47 +0700

    index page. fix order block

--- frontend/views/blocks/order.php
+++ frontend/views/blocks/order.php
@@ -1,34 +1,47 @@
+<?php
+$cur = " р.";
+?>
 <div class="order">
     <div class="container">
         <div class="order-head">
             Популярные модели топливного оборудования
         </div>
         <?php /*<div class="order-stends-title">Стенды для тестирования отечественных и импортных ТНВД</div>*/ ?>
-        <ul class="w-clear order-stend-list owl-carousel owl-theme">
+        <div class="w-clear order-stend-list owl-carousel owl-theme">
             <?php foreach($goods as $good):
                 if($good['is_popular'] == "yes"):?>
-                    <li class="order-stend">
-                        <?php if($good['price'][0]->price != NULL):?>
-                            <div class="order-stend-birk"><?=$good['price'][0]->price?></div>
-                        <?php endif;?>
-
+                    <div class="order-stend">
+                        <div class="tovar__prices">
+                            <?php if($good['price'][0]->price != NULL):?>
+                                <div class="fl-l tovar__price-old gray-bg">
+                                    <div class="crossing">
+                                        <?=number_format($good['price'][0]->price_old, 0, "", " ") . $cur?>
+                                    </div>
+                                </div>
+                            <?php endif;?>
+                            <?php if($good['price'][0]->price != NULL):?>
+                                <div class="tovar__price tovar__price-bg">
+                                    <?=number_format($good['price'][0]->price, 0, "", " ") . $cur?>
+                                </div>
+                            <?php endif;?>
+                        </div>
                         <div class="order-image-wrap">
                             <a href="<?=$good['detailUrl']?>">
                                 <img src="/images/store/<?=$good['mainImage']->filePath?>" class="order-stend-image"/>
                             </a>
                             <div class="order-stend-title"><?=$good['name']?></div>
                         </div>
-
-                        <div class="block-buts">
-                            <a class="order-but fancybox fancybox.ajax" href="zakaz?model=12PSDW">Подробнее</a>
-                        </div>
-                    </li>
-            <?php endif;
+                        <a href="<?=$good['detailUrl']?>" class="order-stend__button but-default">
+                            Подробнее
+                        </a>
+                    </div>
+                <?php endif;
             endforeach;?>
-        </ul>
+        </div>
 
-       <div class="order-notice">А также любые стенды для тестирования Common Rail и ТНВД
-            <a href="zakaz" class="fancybox fancybox.ajax">с фабрики под заказ!</a>
+       <div class="order-notice">
+           А также любые стенды для тестирования Common Rail и ТНВД
+           <a href="zakaz" class="fancybox fancybox.ajax">с фабрики под заказ!</a>
         </div>
     </div>
     <div class="order-bottom"></div>
--- frontend/web/js/common.js
+++ frontend/web/js/common.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 				items: 1
 			},
 			600: {
-				items: 3
+				items: 4
 			}
 		}
 	});
