d526115645fb12f21c2c806027751fd2ac568c38
pash <pashk0s@yandex.ru>
2017-05-03 12:53:20 +0700

    metrika, jivosite and etc.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -27,6 +27,18 @@ $this->beginPage(); ?>
     <?php $this->head() ?>
     <!--script type="text/javascript" async="" src="http://mc.yandex.ru/metrika/watch.js"></script>
     <meta name='yandex-verification' content='7761988de9d78f78' /-->
+    <!-- Traffic tracking code -->
+    <script type="text/javascript">
+        (function(w, p) {
+            var a, s;
+            (w[p] = w[p] || []).push({
+                counter_id: 378028002
+            });
+            a = document.createElement('script'); a.type = 'text/javascript'; a.async = true;
+            a.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'autocontext.begun.ru/analytics.js';
+            s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(a, s);
+        })(window, 'begun_analytics_params');
+    </script>
 </head>
 
 <body>
@@ -140,6 +152,33 @@ $menuItems = [
     });
 </script>
 <?php ?>
+<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter24717443 = new Ya.Metrika({id:24717443, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/24717443" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+<!--script type="text/javascript" src="//perezvoni.com/files/widgets/655-45810d447a-0-0d447a-7544dbc734e1bf458-c734e1bf4581.js" charset="UTF-8"></script-->
+<script type="text/javascript" src="//cdn.perezvoni.com/widget/js/przv.js?przv_code=1636-cd7fc9fdf15-90f17d74cd7fc9fdf15-fdf15-20074d0a90f1" ></script>
+<script src="http://callibri.ru/api/module/js/v1/callibri.js" type="text/javascript"></script>
+<!--Google analytics-->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-69485351-1', 'auto');
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
+
+    /* Accurate bounce rate by time */
+    if (!document.referrer ||
+        document.referrer.split('/')[2].indexOf(location.hostname) != 0)
+        setTimeout(function(){
+            ga('send', 'event', 'Новый посетитель', location.pathname);
+        }, 15000);</script>
+<!--Google analytics-->
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+    (function(){ var widget_id = 'vTd4DpcOsM';
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
 </body>
 </html>
 <?php $this->endPage(); ?>
