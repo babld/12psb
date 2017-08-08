ed3b2214e0a253f2504d28905ae253ff8acfd250
pash <pashk0s@yandex.ru>
2017-08-08 12:15:47 +0700

    fix url on index page

---
+++ frontend/controllers/SiteController.php
@@ -0,0 +1,427 @@
+<?php
+namespace frontend\controllers;
+
+use app\models\SearchForm;
+use yii\web\HttpException;
+use pistol88\shop\models\Category;
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\helpers\Html;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+use pistol88\shop\models\Product;
+use pistol88\shop\models\Image;
+use pistol88\shop\models\Price;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    public function beforeAction($action)
+    {
+        $model = new SearchForm();
+        if($model->load(Yii::$app->request->post()) && $model->validate()) {
+            $query = Html::encode($model->query);
+            return $this->redirect(Yii::$app->urlManager->createUrl(['search', 'query' => $query]));
+        }
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $products = Product::find()->all();
+        $goods = [];
+
+        foreach($products as $good) {
+            $category = [];
+            $mainImage = Image::find()->where(['itemid' => $good->id, 'isMain' => 1])->one();
+            $images = Image::find()->where(['itemid' => $good->id])->all();
+            $price = Price::find()->where(['product_id' => $good->id])->all();
+
+            if(!$mainImage) $mainImage = $images[0];
+            $categoryId = $good->category_id;
+            $categoryData = Category::find()->
+                select('slug, parent_id')->
+                where(['id' => $categoryId])->
+                one();
+
+            $category[] = $categoryData->slug;
+
+            $categoryData = Category::find()->
+                select('slug, parent_id')->
+                where(['id' => $categoryData->parent_id])->
+                one();
+
+            $category[] = $categoryData->slug;
+
+            $category = array_reverse($category);
+
+            $detailUrl = "/";
+            foreach($category as $item) $detailUrl .= $item . '/';
+            $detailUrl .= $good->slug;
+
+            $goods[] = [
+                'mainImage'     => $mainImage,
+                'images'        => $images,
+                'price'         => $price,
+                'is_promo'      => $good->is_promo,
+                'category'      => $category,
+                'detailUrl'     => $detailUrl,
+                'available'     => $good->available,
+                'text'          => $good->text,
+                'short_text'    => $good->short_text,
+                'name'          => $good->name,
+                'is_popular'    => $good->is_popular,
+                'product'       => $good,
+            ];
+        }
+
+        return $this->render('index', [
+            'goods'         => $goods
+        ]);
+    }
+
+    /**
+     * @param $catalog
+     * @return string
+     * @throws HttpException
+     */
+    public function actionCatalog($catalog) {
+        $category = $this->urlcheck($catalog);
+        if($categoryIds = $this->categoryIds($category)){
+            foreach($categoryIds as $id) {
+                $products[] = [
+                    'product' => Product::findAll(['category_id' => $id])
+                ];
+            }
+
+            foreach(array_filter(explode('/', $catalog)) as $item) {
+                $breadcrumbs[] = $this->breadcrumb($item);
+            }
+
+            return $this->render('catalog', [
+                'breadcrumbs' => $breadcrumbs,
+                'products' => $products
+            ]);
+        } else {
+            $path = array_filter(explode('/', $catalog));
+            array_pop($path);
+            foreach($path as $item) {
+                $breadcrumbs[] = $this->breadcrumb($item);
+            }
+
+            return $this->render('view', [
+                'product' => Product::findOne(['slug' => $category]),
+                'breadcrumbs' => $breadcrumbs
+            ]);
+        }
+    }
+
+    public function actionDelivery() {
+        return $this->render('delivery');
+    }
+
+    public function actionZakaz($name) {
+
+        return $this->renderAjax('zakaz', [
+            'name' => $name
+        ]);
+    }
+
+    public function breadcrumb($category) {
+        $id = Category::findOne(['slug' => $category])->id;
+        $link = [];
+        $breadcrumbsname = [];
+        $breadcrumbslinks = [];
+        while($breadcrumb = Category::findOne(['id' => $id])) {
+            $id = $breadcrumb->parent_id;
+            $link[] = $breadcrumb->slug ;
+
+            $breadcrumbsname[] = $breadcrumb->name;
+            $breadcrumbslinks[] = implode("/", array_reverse($link));
+        }
+
+        $breadcrumbs = [
+            'name' => $breadcrumbsname[0],
+            'link' => array_reverse($breadcrumbslinks)[0]
+        ];
+
+        return $breadcrumbs;
+    }
+
+    /*
+     * Проверка урла на сущестование всех подкаталогов (/catalog/stendy-tnvd/dop/ или /asdasdf/stendy/).
+     * Если одного нет то выдает 404
+     */
+    public function urlcheck($catalog) {
+        $i = 0;
+        foreach(array_filter(explode('/', $catalog)) as $item){
+            if(!Category::findOne(['slug' => $item])) {
+                if(!$product = Product::findOne(['slug' => $item]))
+                    throw new HttpException(404 ,'Страница не найдена');
+            } else if($i++ == 0 && Category::findOne(['slug' => $item])->parent_id != NULL) {
+                #Устраняем косяк с открытием одной и той же странице по разным ссылкам
+                # Пример: /catalog/stendy-tnvd и /stendy-tnvd
+                throw new HttpException(404 ,'Страница не найдена');
+            }
+        }
+        return $item;
+    }
+
+    public function categoryIds($category) {
+        # Находим в базе id категории
+        if(!$categoryData = Category::findOne(['slug' => $category])) return null;
+
+        $categoryIds[] = $categoryData->id;
+
+        $this->testids($categoryData->id, $categoryIds);
+
+        return $categoryIds;
+    }
+
+    public function testids($id, &$categoryIds) {
+        #Находим подкатегории текущей категории
+        $subcategories = Category::find()->
+            where(['parent_id' => $id])->
+            all();
+
+        foreach($subcategories as $category) {
+            if($category->parent_id) {
+                $categoryIds[] = $category->id;
+                $this->testids($category->id, $categoryIds);
+            }
+        }
+    }
+
+    public function debug($var) {
+        echo "<pre>";
+        var_dump($var);
+        exit;
+    }
+
+    public function actionView($catalog, $id) {
+        $model = new Product();
+        return $this->render('view', ["id" => $id, 'model' => $model]);
+    }
+
+    public function actionContacts() {
+        return $this->render('contact', [
+
+        ]);
+    }
+
+    public function actionSearch() {
+        $query = Yii::$app->getRequest()->getQueryParam('query');
+
+        $products = Product::find()->where("`name` like '%$query%' or `text` like '%$query%' or `short_text` like '%$query%' or `characteristics` like '%$query%' or `equipment` like '%$query%'")->all();
+
+        return $this->render('search',[
+            'query' => $query,
+            'products' => $products
+        ]);
+    }
+    /**
+     * Logs in a user.
+     *
+     * @return mixed
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        } else {
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Logs out the current user.
+     *
+     * @return mixed
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return mixed
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
+                Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
+            } else {
+                Yii::$app->session->setFlash('error', 'There was an error sending your message.');
+            }
+
+            return $this->refresh();
+        } else {
+            return $this->render('contact', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return mixed
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+
+    public function actionGoogleFeed() {
+        Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
+        Yii::$app->response->headers->add('Content-Type', 'text/xml');
+
+        return $this->renderPartial('google-feed', [
+            'products' => Product::find()->all()
+        ]);
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return mixed
+     */
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                if (Yii::$app->getUser()->login($user)) {
+                    return $this->goHome();
+                }
+            }
+        }
+
+        return $this->render('signup', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Requests password reset.
+     *
+     * @return mixed
+     */
+    public function actionRequestPasswordReset()
+    {
+        $model = new PasswordResetRequestForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail()) {
+                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
+
+                return $this->goHome();
+            } else {
+                Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for the provided email address.');
+            }
+        }
+
+        return $this->render('requestPasswordResetToken', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Resets password.
+     *
+     * @param string $token
+     * @return mixed
+     * @throws BadRequestHttpException
+     */
+    public function actionResetPassword($token)
+    {
+        try {
+            $model = new ResetPasswordForm($token);
+        } catch (InvalidParamException $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
+            Yii::$app->session->setFlash('success', 'New password saved.');
+
+            return $this->goHome();
+        }
+
+        return $this->render('resetPassword', [
+            'model' => $model,
+        ]);
+    }
+}
---
+++ frontend/views/blocks/order.php
@@ -0,0 +1,59 @@
+<?php
+use yii\imagine\Image;
+use pistol88\shop\models\Category;
+use Imagine\Image\Box;
+?>
+<div class="order">
+    <div class="container">
+        <div class="order-head">
+            Популярные модели топливного оборудования
+        </div>
+        <?php /*<div class="order-stends-title">Стенды для тестирования отечественных и импортных ТНВД</div>*/ ?>
+        <div class="order-stend-list owl-carousel owl-theme">
+            <?php foreach($goods as $good):
+                $product = $good['product'];
+                if($good['is_popular'] == "yes"):?>
+                    <div class="order-stend">
+                        <div class="tovar__prices">
+                            <?php if($good['price'][0]->price_old != NULL):?>
+                                <div class="fl-l tovar__price-old gray-bg">
+                                    <div class="crossing">
+                                        <?=number_format($good['price'][0]->price_old, 0, "", " ") . Yii::getAlias('@cur')?>
+                                    </div>
+                                </div>
+                            <?php endif;?>
+                            <?php if($good['price'][0]->price != NULL):?>
+                                <div class="tovar__price tovar__price-bg">
+                                    <?=number_format($good['price'][0]->price, 0, "", " ") . Yii::getAlias('@cur')?>
+                                </div>
+                            <?php endif;?>
+                        </div>
+                        <?php $url = Category::findOne($product->category->id)->getUrl() . '/' .$product->slug; ?>
+                        <div class="order-image-wrap">
+                            <a href="<?= $url ?>"><?php
+                                $width = 264;
+                                $height = 240;
+                                $imagePath = $good['mainImage']->filePath;
+                                $path = explode('/', $imagePath);
+                                $filename = $width.'x'.$height . '-thumb-' . array_pop($path);
+                                $pathToImg = implode('/', $path);
+                                if(!file_exists(Yii::getAlias('@webroot/images/cache/') . $pathToImg . '/' . $filename)) {
+                                    Image::thumbnail(Yii::getAlias('@webroot/images/store/') . $imagePath, $width, $height)
+                                        ->save(Yii::getAlias('@webroot/images/cache/') . $pathToImg . '/' . $filename, ['quality' => 80]);
+                                }?>
+                                <img src="<?='/images/cache/' . $pathToImg . '/' . $filename?>" class="order-stend-image"/>
+                            </a>
+                            <div class="order-stend-title"><?= $good['name'] ?></div>
+                        </div>
+
+                        <a href="<?= $url ?>" class="order-stend__button but-default">
+                            Подробнее
+                        </a>
+                    </div>
+                <?php endif;
+            endforeach;?>
+        </div>
+    </div>
+    <div class="order-bottom"></div>
+
+</div>
