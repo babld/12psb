9a4ee3bbfb451c2dbcb94971aeb10da4660fc790
pash <pashk0s@yandex.ru>
2017-12-21 21:11:23 +0700

    add feedback model

---
+++ frontend/models/FeedbackMessForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * ContactForm is the model behind the contact form.
+ */
+class FeedbackMessForm extends Model
+{
+    public $message;
+    public $phone;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            // name, email, subject and body are required
+            [['phone'], 'required'],
+            ['phone', 'match', 'pattern' => '$^.7.\(\d{3}\).\d{3}.\d{2}.\d{2}$'],
+            ['message', 'string']
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'phone' => 'Телефон',
+            'message' => 'Сообщение'
+        ];
+    }
+}
