3cadf8bd57d49908a4e110c33f057483772573cc
pash <pashk0s@yandex.ru>
2017-12-22 15:08:54 +0700

    adminLTE

---
+++ backend/views/layouts/left.php
@@ -0,0 +1,50 @@
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+            </div>
+            <div class="pull-left info">
+                <p><?= Yii::$app->user->identity->username ?></p>
+
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <?= dmstr\widgets\Menu::widget(
+            [
+                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+                'items' => [
+                    ['label' => 'Menu', 'options' => ['class' => 'header']],
+                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+                    [
+                        'label'=> 'Справочники',
+                        'url' => '#',
+                        'icon' => 'list',
+                        'items'=>[
+                            ['label'=>'Витрина', 'url'=>['/shop/product/index']],
+                            #['label'=>'Промокоды', 'url'=>['/promocode/promo-code/index']],
+                            ['label'=>'Пользователи', 'url'=>['/user/admin/index'],],
+                            ['label' => 'Клиенты', 'url' => ['/client/client/index']]
+                        ]
+                    ],
+                    [
+                        'label'=> 'Поля',
+                        'icon' => 'cog',
+                        'url' => '#',
+                        'items'=>[
+                            ['label' => 'Фильтры', 'url'=>['/filter/filter']],
+                            ['label' => 'Поля магазина', 'url'=>['/field/field']],
+                            ['label' => 'Поля заказа', 'url' => ['/order/field/index']],
+                        ]
+                    ],
+                ],
+            ]
+        ) ?>
+
+    </section>
+
+</aside>
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,119 +1,65 @@
 <?php
+use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use backend\assets\AppAsset;
-use yii\helpers\Html;
-use yii\helpers\Url;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use common\widgets\Alert;
 
-AppAsset::register($this);
-?>
-<?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
+if (Yii::$app->controller->action->id === 'login') { 
+/**
+ * Do not use this code in your template. Remove it. 
+ * Instead, use the code  $this->layout = '//main-login'; in your controller.
+ */
+    echo $this->render(
+        'main-login',
+        ['content' => $content]
+    );
+} else {
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => '<i class="glyphicon glyphicon-home blue" style="color: #c9c9ff;"></i> ' . Yii::$app->name,
-        // 'brandUrl' => Yii::$app->homeUrl,
-        'brandUrl' => Url::to(['/']),
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
+    if (class_exists('backend\assets\AppAsset')) {
+        backend\assets\AppAsset::register($this);
+    } else {
+        app\assets\AppAsset::register($this);
+    }
 
-	if(Yii::$app->user->id) {
-        $navList = [
-            'options' => ['class' => 'navbar-nav navbar-right main-menu'],
-            'encodeLabels' => false,
-            'items'=>[
-                    [
-                        'label'=> 'Отчёты',
-                        'url' => '#',
-                        'items'=>[
-                            ['label'=>'Заказы', 'url'=>['/order/order']],
-                         ]
-                    ],
-                    [
-                        'label'=> 'Справочники',
-                        'url' => '#',
-                        'items'=>[
-                            ['label'=>'Витрина', 'url'=>['/shop/product/index']],
-                            ['label'=>'Промокоды', 'url'=>['/promocode/promo-code/index']],
-                            ['label'=>'Пользователи', 'url'=>['/user/admin/index'],],
-                            ['label' => 'Клиенты', 'url' => ['/client/client/index']]
-                         ]
-                    ],
+    dmstr\web\AdminLteAsset::register($this);
 
-                    [
-                        'label'=> '<i class="glyphicon glyphicon-cog"></i>',
-                        'url' => '#',
-                        'items'=>[
-                            ['label' => 'Фильтры', 'url'=>['/filter/filter']],
-                            ['label' => 'Поля магазина', 'url'=>['/field/field']],
-                            ['label' => 'Поля заказа', 'url' => ['/order/field/index']],
-                         ]
-                    ],
-                    ['label' => yii::$app->user->getIdentity()->username, 'url' => '#', 'icon' => '<i class="fa fa-angle-double-right"></i>'],
-                    '<li>'
-                        . Html::beginForm(['/site/logout'], 'post')
-                        . Html::submitButton(
-                            'Выйти (' . Yii::$app->user->identity->username . ')',
-                            ['class' => 'btn btn-link logout']
-                        )
-                        . Html::endForm()
-                    . '</li>'
-                ]
-        ];
-		echo Nav::widget($navList);
-	} else {
-		echo Nav::widget([
-			'options' => ['class' => 'navbar-nav navbar-right'],
-			'items' => [
-				[
-					'label' => 'Вход',
-					'url' => '/user/login',
-				],
-			]
-		]);
-	}
-
-    NavBar::end();
+    $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
     ?>
+    <?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+    <?php $this->beginBody() ?>
+    <div class="wrapper">
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+        <?= $this->render(
+            'header.php',
+            ['directoryAsset' => $directoryAsset]
+        ) ?>
+
+        <?= $this->render(
+            'left.php',
+            ['directoryAsset' => $directoryAsset]
+        )
+        ?>
+
+        <?= $this->render(
+            'content.php',
+            ['content' => $content, 'directoryAsset' => $directoryAsset]
+        ) ?>
+
     </div>
-</div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
-
-<?php $this->endBody() ?>
-</body>
-</html>
-<?php $this->endPage() ?>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+    <?php $this->endPage() ?>
+<?php } ?>
