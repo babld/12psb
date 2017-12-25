d84fabfc360247a3759893286fc4bebf0302e648
Kokorin Pavel <twrkpmail@gmail.com>
2017-12-25 11:44:17 +0700

    yml fix

--- frontend/views/site/yml.php
+++ frontend/views/site/yml.php
@@ -2,7 +2,6 @@
     <shop>
 		<name>12PSB.RU</name>
 		<company>12PSB.RU</company>
-		<title>Стенды ТНВД и Common Rail по низким ценам</title>
 		<url>http://12psb.ru</url>
 		<currencies>
 			<currency id="RUB" rate="1"/>
@@ -15,17 +14,20 @@
 			<?php foreach($products as $product) :?>
 				<offer id='<?=$product->id?>'>
 					<url>http://12psb.ru</url>
+					<name><?=$product->name?></name>
+					<model><?=$product->name?></model>
 					<price><?=number_format($product->price, 0, '','')?></price>
 					<currencyId>RUB</currencyId>
+					<local_delivery_cost>0</local_delivery_cost>
 					<categoryId>2</categoryId>
 					<vendor>12PSB.RU</vendor>
-					<vendorCode>12PSB.RU</vendorCode>
-					<model><?=$product->name?></model>
+					<vendorCode>12PSB.RU</vendorCode>					
 					<param name="Артикул"><?=$product->code?></param>
 					<description><?=html_entity_decode(strip_tags($product->short_text))?></description>
-					<picture><?php foreach($product->getImages() as $image) : ?>
-                                                http://12psb.ru/images/store/<?= $image->filePath ?>
-                                            <?php endforeach; ?>
+					<picture>
+					<?php foreach($product->getImages() as $image) : ?>
+					http://12psb.ru/images/store/<?= $image->filePath ?>
+                    <?php endforeach; ?>
 					</picture>
 				</offer>
 			<?php endforeach; ?>
